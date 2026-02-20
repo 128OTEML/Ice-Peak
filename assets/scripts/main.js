1bb927110fcb1229fe9fca405405d32dcd037434
Administrator <3693005811@qq.com>
2026-02-20 18:12:19 +0800

    合并json模组

---
+++ assets/scripts/main.js
@@ -0,0 +1,15 @@
+Vars.maxSchematicSize = 128;
+if (!Vars.headless) {
+	Vars.renderer.minZoom = 0.1;
+	Vars.renderer.maxZoom = 256;
+}
+MapResizeDialog.minSize = 1
+MapResizeDialog.maxSize = 1200
+
+require("icepeak/lib");
+require("icepeak/0m-00-puller");
+require("icepeak/0m-01-pusher");
+
+require("multi-crafter");
+require("hud");
+require("sectorSize");
